49961cf48625cfa5562843f99630bbc14d303e62
MoeAlkhrashi <moe.alkhrashi@slingshot.xyz>
2024-06-13 17:01:10 -0400

    moved endpoint test script to scripts/

--- scripts/make_example_requests.py
+++ scripts/make_example_requests.py
@@ -1,3 +1,13 @@
+"""
+This is a simple script to test that makes test requests to the service
+endponts with an example input.
+
+To run this script:
+1. Start the FastAPI server (could use fastapi dev on the root of the repo)
+2. Run python scripts/make_example_requests.py
+3. If the script runs correctly, results will be be saved to "results".
+Otherwise an error will be printed.
+"""
 import os
 
 import requests
@@ -20,8 +30,8 @@ def write_to_file(filename, content):
 anonymize_response = requests.post("http://127.0.0.1:8000/anonymize", json={"text": example})
 if anonymize_response.status_code == 200:
     anonymized_data = anonymize_response.json()
-    write_to_file('anonymized_text.txt', anonymized_data["anonymized_text"])
-    write_to_file('replacement_dict.txt', str(anonymized_data["replacement_dict"]))
+    write_to_file('anonymized_text.txt', anonymized_data["anonymizedText"])
+    write_to_file('replacement_dict.txt', str(anonymized_data["replacementDict"]))
 else:
     print("Anonymize request failed:", anonymize_response.status_code, anonymize_response.text)
 
@@ -29,11 +39,11 @@ else:
 if anonymize_response.status_code == 200:
     deanonymize_response = requests.post(
         "http://127.0.0.1:8000/deanonymize",
-        json={"text": anonymized_data["anonymized_text"], "replacement_dict": anonymized_data["replacement_dict"]},
+        json={"text": anonymized_data["anonymizedText"], "replacementDict": anonymized_data["replacementDict"]},
     )
     if deanonymize_response.status_code == 200:
         deanonymized_data = deanonymize_response.json()
-        write_to_file('deanonymized_text.txt', deanonymized_data["deanonymized_text"])
+        write_to_file('deanonymized_text.txt', deanonymized_data["deanonymizedText"])
     else:
         print("Deanonymize request failed:", deanonymize_response.status_code, deanonymize_response.text)
 
@@ -41,7 +51,7 @@ if anonymize_response.status_code == 200:
 pseudoanonymize_response = requests.post("http://127.0.0.1:8000/pseudoanonymize", json={"text": example})
 if pseudoanonymize_response.status_code == 200:
     pseudoanonymize_data = pseudoanonymize_response.json()
-    write_to_file('pseudoanonymized_anonymized_text.txt', pseudoanonymize_data["anonymized_text"])
-    write_to_file('pseudoanonymized_deanonymized_text.txt', pseudoanonymize_data["deanonymized_text"])
+    write_to_file('pseudoanonymized_anonymized_text.txt', pseudoanonymize_data["anonymizedText"])
+    write_to_file('pseudoanonymized_deanonymized_text.txt', pseudoanonymize_data["deanonymizedText"])
 else:
     print("Pseudoanonymize request failed:", pseudoanonymize_response.status_code, pseudoanonymize_response.text)
